4b7afa38c22aad9512d9ec7ce6e0b2d8c62c4221
ethan-liberfit <ethan.rucar@liber.fit>
2025-02-11 14:12:24 +0100

    a single sprite

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,27 @@ use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::render::{WindowCanvas, Texture};
+use sdl2::rect::{Point, Rect};
 // "self" imports the "image" module itself as well as everything else we listed
 use sdl2::image::{self, LoadTexture, InitFlag};
 use std::time::Duration;
 
-fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture) -> Result<(), String> {
+fn render(
+    canvas: &mut WindowCanvas, 
+    color: Color, 
+    texture: &Texture,
+    position: Point,
+    sprite: Rect
+) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
 
-    _ = canvas.copy(texture, None, None);
+    let (width, height) = canvas.output_size()?;
+
+    // Treat the center of the screen as the (0, 0) coordinate
+    let screen_position = position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
+    _ = canvas.copy(texture, sprite, screen_rect);
 
     canvas.present();
 
@@ -37,6 +49,10 @@ fn main() -> Result<(), String> {
     let texture_creator = canvas.texture_creator();
     let texture = texture_creator.load_texture("assets/bardo.png")?;
 
+    let position = Point::new(0, 0);
+    // src position in the spritesheet
+    let sprite = Rect::new(0, 0, 26, 36);
+
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
 
@@ -56,7 +72,7 @@ fn main() -> Result<(), String> {
         i = (i + 1) % 255;
 
         // Render
-        _ = render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture);
+        _ = render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, position, sprite);
 
         // Time management
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
