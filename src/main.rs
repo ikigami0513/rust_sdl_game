548d996a633f9a85892ef4acad6792c380011e99
ethan-liberfit <ethan.rucar@liber.fit>
2025-02-11 15:38:48 +0100

    Animation

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ enum Direction {
     Up,
     Down,
     Left,
-    Right
+    Right,
 }
 
 #[derive(Debug)]
@@ -23,29 +23,52 @@ struct Player {
     sprite: Rect,
     speed: i32,
     direction: Direction,
+    current_frame: i32,
+}
+
+/// Returns the row of the spritesheet corresponding to the given direction
+fn direction_spritesheet_row(direction: Direction) -> i32 {
+    use self::Direction::*;
+    match direction {
+        Up => 3,
+        Down => 0,
+        Left => 1,
+        Right => 2,
+    }
 }
 
 fn render(
-    canvas: &mut WindowCanvas, 
-    color: Color, 
+    canvas: &mut WindowCanvas,
+    color: Color,
     texture: &Texture,
-    player: &Player
+    player: &Player,
 ) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
 
     let (width, height) = canvas.output_size()?;
 
+    let (frame_width, frame_height) = player.sprite.size();
+    let current_frame = Rect::new(
+        player.sprite.x() + frame_width as i32 * player.current_frame,
+        player.sprite.y() + frame_height as  i32 * direction_spritesheet_row(player.direction),
+        frame_width,
+        frame_height,
+    );
+
     // Treat the center of the screen as the (0, 0) coordinate
     let screen_position = player.position + Point::new(width as i32 / 2, height as i32 / 2);
-    let screen_rect = Rect::from_center(screen_position, player.sprite.width(), player.sprite.height());
-    _ = canvas.copy(texture, player.sprite, screen_rect);
+    let screen_rect = Rect::from_center(screen_position, frame_width, frame_height);
+    canvas.copy(texture, current_frame, screen_rect)?;
 
     canvas.present();
 
     Ok(())
 }
 
+// Update player a fixed amount based on their speed.
+// WARNING: Calling this function too often or at a variable speed will cause the player's speed
+// to be unpredictable!
 fn update_player(player: &mut Player) {
     use self::Direction::*;
     match player.direction {
@@ -60,20 +83,25 @@ fn update_player(player: &mut Player) {
         },
         Down => {
             player.position = player.position.offset(0, player.speed);
-        }
+        },
+    }
+
+    // Only continue to animate if the player is moving
+    if player.speed != 0 {
+        // Cheat: using the fact that all animations are 3 frames (NOT extensible)
+        player.current_frame = (player.current_frame + 1) % 3;
     }
 }
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
-
     // Leading "_" tells Rust that this is an unused variable that we don't care about. It has to
     // stay unused because if we don't have any variable at all then Rust will treat it as a
     // temporary value and drop it right away!
-    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG);
+    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
 
-    let window = video_subsystem.window("rust sdl game", 800, 600)
+    let window = video_subsystem.window("game tutorial", 800, 600)
         .position_centered()
         .build()
         .expect("could not initialize video subsystem");
@@ -89,16 +117,16 @@ fn main() -> Result<(), String> {
         sprite: Rect::new(0, 0, 26, 36),
         speed: 0,
         direction: Direction::Right,
+        current_frame: 0,
     };
 
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
-
     'running: loop {
         // Handle events
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit { .. } |
+                Event::Quit {..} |
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                     break 'running;
                 },
@@ -133,9 +161,9 @@ fn main() -> Result<(), String> {
         update_player(&mut player);
 
         // Render
-        _ = render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, &player);
+        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, &player)?;
 
-        // Time management
+        // Time management!
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 20));
     }
 
