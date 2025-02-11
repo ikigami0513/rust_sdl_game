5dfa2ddc13d2212f67f431ffc9da2651d25ba843
ethan-liberfit <ethan.rucar@liber.fit>
2025-02-11 14:20:39 +0100

    refactor: player struct

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,17 @@ use sdl2::rect::{Point, Rect};
 use sdl2::image::{self, LoadTexture, InitFlag};
 use std::time::Duration;
 
+#[derive(Debug)]
+struct Player {
+    position: Point,
+    sprite: Rect,
+}
+
 fn render(
     canvas: &mut WindowCanvas, 
     color: Color, 
     texture: &Texture,
-    position: Point,
-    sprite: Rect
+    player: &Player
 ) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
@@ -20,9 +25,9 @@ fn render(
     let (width, height) = canvas.output_size()?;
 
     // Treat the center of the screen as the (0, 0) coordinate
-    let screen_position = position + Point::new(width as i32 / 2, height as i32 / 2);
-    let screen_rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
-    _ = canvas.copy(texture, sprite, screen_rect);
+    let screen_position = player.position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(screen_position, player.sprite.width(), player.sprite.height());
+    _ = canvas.copy(texture, player.sprite, screen_rect);
 
     canvas.present();
 
@@ -49,9 +54,10 @@ fn main() -> Result<(), String> {
     let texture_creator = canvas.texture_creator();
     let texture = texture_creator.load_texture("assets/bardo.png")?;
 
-    let position = Point::new(0, 0);
-    // src position in the spritesheet
-    let sprite = Rect::new(0, 0, 26, 36);
+    let player = Player {
+        position: Point::new(0, 0),
+        sprite: Rect::new(0, 0, 26, 36)
+    };
 
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
@@ -72,7 +78,7 @@ fn main() -> Result<(), String> {
         i = (i + 1) % 255;
 
         // Render
-        _ = render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, position, sprite);
+        _ = render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, &player);
 
         // Time management
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
